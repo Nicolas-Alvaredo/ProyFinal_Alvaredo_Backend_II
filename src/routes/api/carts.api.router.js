a7c4be8433d9d96043cbeebd51a54b51eaad848a
Nicolas Alvaredo <nicoalvaredo_utn@hotmail.com>
2025-05-09 00:48:53 -0300

    Agregamos logica de roles de  users y carrito

--- src/routes/api/carts.api.router.js
+++ src/routes/api/carts.api.router.js
@@ -1,17 +1,74 @@
 import express from "express";
+import {
+  createCart,
+  getCartById,
+  addProductToCart,
+  removeProductFromCart,
+  updateCart,
+  updateProductQuantity,
+  clearCart,
+  getAllCarts
+} from "../../controllers/cart.controller.js";
 import passport from "../../middlewares/passport-jwt.middleware.js";
 import { checkRole } from "../../middlewares/checkRole.js";
-import {
-  addProductToCart,
-} from "../../controllers/cart.controller.js";
 
 const router = express.Router();
+
+// ✅ Rutas protegidas para usuarios autenticados (rol: user)
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  checkRole("user"),
+  createCart
+);
+
+router.get(
+  "/:cid",
+  passport.authenticate("jwt", { session: false }),
+  checkRole("user"),
+  getCartById
+);
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  checkRole("user"),
+  getAllCarts
+);
 
 router.post(
   "/:cid/products/:pid",
   passport.authenticate("jwt", { session: false }),
   checkRole("user"),
   addProductToCart
+);
+
+router.delete(
+  "/:cid/products/:pid",
+  passport.authenticate("jwt", { session: false }),
+  checkRole("user"),
+  removeProductFromCart
+);
+
+router.put(
+  "/:cid",
+  passport.authenticate("jwt", { session: false }),
+  checkRole("user"),
+  updateCart
+);
+
+router.put(
+  "/:cid/products/:pid",
+  passport.authenticate("jwt", { session: false }),
+  checkRole("user"),
+  updateProductQuantity
+);
+
+router.delete(
+  "/:cid",
+  passport.authenticate("jwt", { session: false }),
+  checkRole("user"),
+  clearCart
 );
 
 export default router;
